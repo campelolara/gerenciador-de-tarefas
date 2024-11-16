2bf0fca9e963217202d17d3c6ff911f78e1564a6
Lara <laracampelo505@gmail.com>
2024-11-15 22:46:45 -0300

    main

--- solucoes-ambientais/main.c
+++ solucoes-ambientais/main.c
@@ -1,41 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+
 //registro das industrias
-    typedef struct{
-        char nome[50];
-        char cpf[15];
-        char rg[20];
-        char nomeEmpresa[50];
-        char cnpj[19];
-        char razaoSocial[151];
-        char dataAbertura[11];
-        char rua[101];
-        char numero[11];
-        char bairro[51];
-        char cidade[51];
-        char estado[3];
-        char cep[10];
-    }industria;
+typedef struct{
+    char nome[151];
+    char cpf[15];
+    char rg[20];
+    char nomeEmpresa[50];
+    char cnpj[19];
+    char dataAbertura[11];
+    char rua[101];
+    char numero[11];
+    char bairro[51];
+    char cidade[51];
+    char estado[3];
+    char cep[10];
+    char email[101];
+    char telefone[16];
+}industria;
 
-int main()
-{
-    //suporte acentuação
+//corpo principal
+int main(){
     setlocale(LC_ALL, "Portuguese");
-
     int escolha = 1;
-
-
 
     //tela inicial (após cadastro)
     printf("--------------------------------------\n");
     printf("             TELA INICIAL               \n");
     printf("--------------------------------------\n");
-    printf("1. ver dados das indústrias\n");
-    printf("2. cadastrar uma nova indústria\n");
-    printf("3. atualizar dados mensais de resíduos\n");
-    printf("4. gerar relatórios\n");
-    printf("5. sair\n");
+    printf("1. Ver dados das indústrias\n");
+    printf("2. Cadastrar uma nova indústria\n");
+    printf("3. Atualizar dados mensais de resíduos\n");
+    printf("4. Gerar relatórios\n");
+    printf("5. Sair\n");
 
     while(escolha){
         printf("Escolha uma opção: ");
@@ -60,8 +58,5 @@ int main()
                 break;
         }
     }
-
-
-
     return 0;
 }
